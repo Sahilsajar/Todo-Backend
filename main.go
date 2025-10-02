ab33cea2335d2bf2271523bcd9f1552049326dff
Sahilsajar <sahilsajar13890@gmail.com>
2025-10-02 17:40:17 +0530

    package api

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,9 +18,13 @@ if use small letter then it will become private and c.json will not able to expo
 /*
 Without json tag it will also work but mainly in DB camelcase or snakecase so that is why  we explicitly provide names for it.
 */
+
+/*
+If we dont put binding and wrong json or no json is provided still it will add with default values.
+*/
 type todo struct {
-	Id        int    `json:"id"`
-	Task      string `json:"task"`
+	Id        int    `json:"id" binding:"required"`
+	Task      string `json:"task" binding:"required"`
 	Completed bool   `json:"completed"`
 }
 
@@ -32,8 +38,37 @@ func getTodos(c *gin.Context) {
 	c.JSON(200, todos)
 }
 
+func addTodo(c *gin.Context) {
+	var newTodo todo
+
+	if Err := c.ShouldBindJSON(&newTodo); Err != nil {
+		c.JSON(400, gin.H{"message": Err.Error()})
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	c.JSON(201, todos)
+
+}
+
+func markComplete(c *gin.Context) {
+	newId, _ := strconv.Atoi(c.Param("id"))
+	for i := 0; i < len(todos); i++ {
+		if todos[i].Id == newId {
+			todos[i].Completed = true
+			c.JSON(202, todos)
+			return
+		}
+	}
+	c.JSON(400, gin.H{"message": "No task found"})
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/getTodos", getTodos)
+	r.POST("/addTodo", addTodo)
+	r.PATCH("/over/:id", markComplete)
+
 	r.Run("localhost:8080")
 }
